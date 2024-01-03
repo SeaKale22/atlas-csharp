a495ba74f8c3dcd2ef312523964701a2a146669f
SeaKale22 <kaleb.tahji.k@gmail.com>
2024-01-03 15:36:05 -0600

    prints a string three times followed by a substring of the strings first nine characters, then pritns a new line

---
+++ csharp-hello_world/6-print_string/6-print_string.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		string str = "Holberton School";
+        string substr = str.Substring(0, 9);
+		Console.WriteLine($"{str}{str}{str}{substr}");
+        }
+}
