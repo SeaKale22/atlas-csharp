8fd00604a8bb280a10cfdfbab9ca4c6523d374be
SeaKale22 <kaleb.tahji.k@gmail.com>
2024-01-04 11:12:16 -0600

    prints numbers 0 - 99 in specific format

---
+++ csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _5_print_comb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 99; i++)
+            {
+                if (i < 99)
+                {
+                    Console.Write($"{i:d2}, ");
+                }
+                else
+                {
+                    Console.WriteLine($"{i:d2}");
+                }
+            }
+        }
+    }
+}
