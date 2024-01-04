5ca878b6ba8468e8b9e8c9d723211f4e96de000e
SeaKale22 <kaleb.tahji.k@gmail.com>
2024-01-04 11:55:04 -0600

    prints lowest combination of each digit

---
+++ csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _6_print_comb2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 9; i++)
+            {
+                for (int j = i + 1; j <= 9; j++)
+                {
+                    if (i != j)
+                    {
+                        Console.Write($"{i}{j}");
+                        if (i == 8 && j == 9)
+                        {
+                            Console.WriteLine("");
+                        }
+                        else
+                        {
+                            Console.Write(", ");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
