4a2e5a1f6b4cf28b1639311bc62ca811836c418b
SeaKale22 <kaleb.tahji.k@gmail.com>
2024-01-04 10:48:35 -0600

    prints alphabet except for e and q

---
+++ csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _3_print_alphabt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char alph = 'a';
+            while (alph <= 'z')
+            {
+                if (alph == 'q' || alph == 'e')
+                {
+                    alph++;
+                    continue;
+                }
+                Console.Write(alph);
+                alph++;
+            }
+        }
+    }
+}
