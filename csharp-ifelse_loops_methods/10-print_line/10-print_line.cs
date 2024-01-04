353a918e8b364cdfca36a99c7fb1afc5dfbafd83
SeaKale22 <kaleb.tahji.k@gmail.com>
2024-01-04 14:05:56 -0600

    class Line with method PrintLine

---
+++ csharp-ifelse_loops_methods/10-print_line/10-print_line.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Line
+{
+    public static void PrintLine(int length)
+    {
+        for (int i = 1; i <= length; i++)
+        {
+            Console.Write("_");
+        }
+        Console.WriteLine();
+    }
+}
