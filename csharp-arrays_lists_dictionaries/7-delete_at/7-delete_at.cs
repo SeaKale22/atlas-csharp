57ebb3923242470be86844a30ea757cc4422f651
SeaKale22 <kaleb.tahji.k@gmail.com>
2024-01-04 19:31:32 -0600

    deletes at specific index

---
+++ csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class List
+{
+    public static List<int> DeleteAt(List<int> myList, int index)
+    {
+        if (index < 0 || index >= myList.Count)
+        {
+            Console.WriteLine("Index is out of range");
+            return myList;
+        }
+        for (int i = index; i < myList.Count - 1; i++)
+        {
+            myList[i] = myList[i + 1];
+        }
+        myList.RemoveAt(myList.Count - 1);
+        return myList;
+    }
+}
