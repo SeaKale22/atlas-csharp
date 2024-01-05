850801def3ea9d46890c4cc08702109e52d7fd9e
SeaKale22 <kaleb.tahji.k@gmail.com>
2024-01-05 12:03:54 -0600

    searches, pops, and adds a stack

---
+++ csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+
+class MyStack
+{
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+    {
+        int stack_count = aStack.Count;
+        Console.WriteLine($"Number of items: {stack_count}");
+        
+        if (stack_count == 0)
+        {
+            Console.WriteLine("Stack is empty");
+        }
+        else
+        {
+            Console.WriteLine($"Top item: {aStack.Peek()}");
+        }
+
+        Console.WriteLine($"Stack contains {search}: {aStack.Contains(search)}");
+
+        if (aStack.Contains(search))
+        {
+            while (aStack.Contains(search))
+            {
+                aStack.Pop();
+            }
+        }
+
+        aStack.Push(newItem);
+        return aStack;
+    }
+}
