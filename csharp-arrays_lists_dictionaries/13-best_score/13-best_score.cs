bc378ef8daff24069c65c6cb633c69ad98c8fbfe
SeaKale22 <kaleb.tahji.k@gmail.com>
2024-01-04 21:01:31 -0600

    fixed for score = 0 and for empty dict

--- csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
+++ csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -5,8 +5,12 @@ class Dictionary
 {
     public static string BestScore(Dictionary<string, int> myList)
     {
+        if (myList.Count == 0)
+        {
+            return "None";
+        }
         string high_score = "";
-        int currentscore = 0;
+        int currentscore = -1;
         foreach (KeyValuePair<string, int> kvp in myList)
         {
             if (kvp.Value > currentscore)
