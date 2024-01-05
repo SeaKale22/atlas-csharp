dda8bfb007f125a5d61efef0738b803c0d6bb714
SeaKale22 <kaleb.tahji.k@gmail.com>
2024-01-04 21:17:35 -0600

    creats and prints a 5 by 5 2d array

---
+++ csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _14_rectangular_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] rect = new int[5, 5];
+            rect[2, 2] = 1;
+            for (int col = 0; col < 5; col++)
+            {
+                for (int row = 0; row < 5; row++)
+                {
+                    Console.Write($"{rect[col, row]}");
+                    if (row < 4)
+                    {
+                        Console.Write(" ");    
+                    }
+                    else
+                    {
+                        Console.WriteLine();
+                    }
+                }
+            }
+        }
+    }
+}
