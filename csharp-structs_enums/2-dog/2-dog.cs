86d48ee2c1f38cd1d616afc39471c1b7b0ae337a
SeaKale22 <kaleb.tahji.k@gmail.com>
2024-01-09 11:06:58 -0600

    adds constructor

---
+++ csharp-structs_enums/2-dog/2-dog.cs
@@ -0,0 +1,24 @@
+using System;
+
+public enum Rating
+{
+    Good,
+    Great,
+    Excellent
+}
+
+public struct Dog
+{
+    public string name;
+    public float age;
+    public string owner;
+    public Rating rating;
+
+    public Dog(string Name, float Age, string Owner, Rating Rating)
+    {
+        name = Name;
+        age = Age;
+        owner = Owner;
+        rating = Rating;
+    }
+}
