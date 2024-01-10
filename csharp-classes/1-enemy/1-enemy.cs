8ace0037673f70f82379ef1e017115d867e205f0
SeaKale22 <kaleb.tahji.k@gmail.com>
2024-01-10 12:44:01 -0600

    adds health field and constructor that sets health to 0 to Zombie class.

---
+++ csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Enemies
+{
+    /// <summary>Defines a Zombie</summary>
+    public class Zombie
+    {
+        /// <summary>Represents Zombies Health</summary>
+        public int health;
+
+        /// <summary>Zombie Constructor</summary>
+        public Zombie()
+        {
+            this.health = 0;
+        }
+    }
+}
