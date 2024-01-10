7b967557dffa43c6239a8a8d1a18ab8c697f6ee2
SeaKale22 <kaleb.tahji.k@gmail.com>
2024-01-10 12:21:25 -0600

    updated documentation to be in the correct area

--- csharp-classes/0-enemy/0-enemy.cs
+++ csharp-classes/0-enemy/0-enemy.cs
@@ -2,10 +2,8 @@ using System;
 
 namespace Enemies
 {
+    /// <summary>Defines a Zombie</summary>
     public class Zombie
     {
-        /// <summary>
-        /// Defines a Zombie
-        /// </summary>
     }
 }
