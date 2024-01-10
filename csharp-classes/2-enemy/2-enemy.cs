bb26203e0eea2cf3437e0274d4f7ac52c1d57d18
SeaKale22 <kaleb.tahji.k@gmail.com>
2024-01-10 13:17:00 -0600

    fixed issue where exeption would be thown when constructor is passed 0

--- csharp-classes/2-enemy/2-enemy.cs
+++ csharp-classes/2-enemy/2-enemy.cs
@@ -17,7 +17,7 @@ namespace Enemies
         /// <summary>Zombie Constructor with int param</summary>
         public Zombie(int value)
         {
-            if (value <= 0)
+            if (value < 0)
             {
                 throw new ArgumentException("Health must be greater than or equal to 0");
             }
