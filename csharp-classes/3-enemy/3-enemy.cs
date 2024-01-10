268b9c43b65634c29afad1fdf6d646f4843c1043
SeaKale22 <kaleb.tahji.k@gmail.com>
2024-01-10 13:25:03 -0600

    health is now private and added GetHealth that returns the Zombies health

---
+++ csharp-classes/3-enemy/3-enemy.cs
@@ -0,0 +1,37 @@
+using System;
+
+namespace Enemies
+{
+    /// <summary>Defines a Zombie</summary>
+    public class Zombie
+    {
+        /// <summary>Represents Zombies Health</summary>
+        private int health;
+
+        /// <summary>Zombie Constructor</summary>
+        public Zombie()
+        {
+            this.health = 0;
+        }
+
+        /// <summary>
+        /// Zombie Constructor with int param
+        /// </summary>
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            this.health = value;
+        }
+
+        /// <summary>
+        /// returns health of the zombie
+        /// </summary>
+        public int GetHealth()
+        {
+            return this.health;
+        }
+    }
+}
