9f58d9ccdcdaea83b1ca6ebb74340775e038ba74
SeaKale22 <kaleb.tahji.k@gmail.com>
2024-01-15 14:32:35 -0600

    added more test cases

--- csharp-tdd/2-max_int/MyMath.Tests/MyMath.Tests.cs
+++ csharp-tdd/2-max_int/MyMath.Tests/MyMath.Tests.cs
@@ -11,9 +11,33 @@ namespace MyMath.Tests
         }
 
         [Test]
-        public void Max_NormalUse_ReturnsMaxInt()
+        public void Max_MaxMiddle_ReturnsMaxInt()
         {
             List<int> nums = new List<int> { 2, 4, 8, 3, 1};
+            int max = Operations.Max(nums);
+            Assert.AreEqual(8, max);        
+        }
+
+        [Test]
+        public void Max_MaxEnd_ReturnsMaxInt()
+        {
+            List<int> nums = new List<int> { 2, 4, 1, 3, 8};
+            int max = Operations.Max(nums);
+            Assert.AreEqual(8, max);        
+        }
+
+         [Test]
+        public void Max_MaxStart_ReturnsMaxInt()
+        {
+            List<int> nums = new List<int> { 8, 4, 2, 3, 1};
+            int max = Operations.Max(nums);
+            Assert.AreEqual(8, max);        
+        }
+
+        [Test]
+        public void Max_ContainsNegitive_ReturnsMaxInt()
+        {
+            List<int> nums = new List<int> { 2, -4, 8, 3, 1};
             int max = Operations.Max(nums);
             Assert.AreEqual(8, max);        
         }
