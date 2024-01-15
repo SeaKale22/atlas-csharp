f9360d6551a64f58f2d2837c659e7d58c267b296
SeaKale22 <kaleb.tahji.k@gmail.com>
2024-01-15 15:57:15 -0600

    checks if string is palindrome with test cases

---
+++ csharp-tdd/3-palindrome/Text.Tests/Text.Tests.cs
@@ -0,0 +1,47 @@
+using NUnit.Framework;
+
+namespace Text.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void IsPalindrome_SingleWordCaps_RetunsTrue()
+        {
+            string str = "Racecar";
+            Assert.IsTrue(Str.IsPalindrome(str));
+        }
+
+        [Test]
+        public void IsPalindrome_SingleWord_RetunsTrue()
+        {
+            string str = "level";
+            Assert.IsTrue(Str.IsPalindrome(str));
+        }
+
+        [Test]
+        public void IsPalindrome_HasPunctuation_ReturnsTrue()
+        {
+            string str = "A man, a plan, a canal: Panama.";
+            Assert.IsTrue(Str.IsPalindrome(str));
+        }
+
+        [Test]
+        public void IsPalindrome_SingleWord_RetunsFalse()
+        {
+            string str = "kitten";
+            Assert.IsFalse(Str.IsPalindrome(str));
+        }
+
+        [Test]
+        public void IsPalindrome_HasPunctuation_ReturnsFalse()
+        {
+            string str = "Welp, I got nothing; same as you";
+            Assert.IsFalse(Str.IsPalindrome(str));
+        }
+    }
+}
---
+++ csharp-tdd/3-palindrome/Text/Text.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace Text
+{
+    /// <summary>
+    /// operations for strings
+    /// </summary>
+    public class Str
+    {
+        /// <summary> determins if string is a palindrome </summary>
+        public static bool IsPalindrome(string s)
+        {
+            string convertedString = new string(s.ToLower().ToCharArray().Where(c => Char.IsLetterOrDigit(c)).ToArray());
+            string reversedString = new string(convertedString.Reverse().ToArray());
+            return convertedString == reversedString;
+        }
+    }
+}
