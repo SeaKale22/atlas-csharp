004c0f5fd8cd5b10076f618d7eff3c9d174948aa
SeaKale22 <kaleb.tahji.k@gmail.com>
2024-01-16 13:20:24 -0600

    counts number or words in camelcase string with test cases

---
+++ csharp-tdd/5-camelcase/Text.Tests/Text.Tests.cs
@@ -0,0 +1,33 @@
+using NUnit.Framework;
+
+namespace Text.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void CamelCase_OneWord_ReturnsOne()
+        {
+            int count = Str.CamelCase("word");
+            Assert.AreEqual(1, count);
+        }
+
+        [Test]
+        public void CamelCase_MultiWord_ReturnsCount()
+        {
+            int count = Str.CamelCase("thisIsAString");
+            Assert.AreEqual(4, count);
+        }
+
+        [Test]
+        public void CamelCase_EmptyString_ReturnsZero()
+        {
+            int count = Str.CamelCase("");
+            Assert.AreEqual(0, count);
+        }
+    }
+}
---
+++ csharp-tdd/5-camelcase/Text/Text.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Text
+{
+    /// <summary>
+    /// String opertations
+    /// </summary>
+    public class Str
+    {
+        /// <summary> How Many Words in string </summary>
+        public static int CamelCase(string s)
+        {
+            int count = 0;
+            if (s.Length == 0)
+            {
+                return count;
+            }
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (char.IsUpper(s[i]))
+                {
+                    count++;
+                }
+            }
+            return count + 1;
+        }
+    }
+}
