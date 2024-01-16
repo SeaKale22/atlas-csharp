3e566d2a72aa1b966d521fdc258c6f0e04ab401e
SeaKale22 <kaleb.tahji.k@gmail.com>
2024-01-16 15:45:43 -0600

    adds new area method and overides ToString method for Rectangle

---
+++ csharp-inheritance/7-shape/7-shape.cs
@@ -0,0 +1,64 @@
+using System;
+
+/// <summary> represents a shape </summary>
+class Shape
+{
+    /// <summary> area method not implemented </summary>
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
+
+/// <summary> represents a rectangle </summary>
+class Rectangle : Shape
+{
+    private int width;
+    private int height;
+
+    /// <summary> width property </summary>
+    public int Width
+    {
+        get {return this.width;}
+        set 
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Width must be greater than or equal to 0");
+            }
+            else
+            {
+                this.width = value;
+            }
+        }
+    }
+
+    /// <summary> height propety </summary>
+    public int Height
+    {
+        get {return this.height;}
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Height must be greater than or equal to 0");
+            }
+            else
+            {
+                this.height = value;
+            }
+        }
+    }
+
+    /// <summary> finds area of the rectangle </summary>
+    public new int Area()
+    {
+        return this.height * this.width;
+    }
+
+    /// <summary> overrides toString </summary>
+    public override string ToString()
+    {
+        return $"[Rectangle] {this.width} / {this.height}";
+    }
+}
