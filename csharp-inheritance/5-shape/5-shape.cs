7574830b9130ce8af44972ef3b7b191f0d321753
SeaKale22 <kaleb.tahji.k@gmail.com>
2024-01-16 15:21:04 -0600

    class shape with unimplemented area method

---
+++ csharp-inheritance/5-shape/5-shape.cs
@@ -0,0 +1,11 @@
+using System;
+
+/// <summary> represents a shape </summary>
+class Shape
+{
+    /// <summary> area method not implemented </summary>
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
