c2538ff6e04d7e10010d3011c40d0788158af338
SeaKale22 <kaleb.tahji.k@gmail.com>
2024-01-17 15:39:15 -0600

    fixed typos in Interact method of Decoration class

--- csharp-interfaces/3-decorations/3-decorations.cs
+++ csharp-interfaces/3-decorations/3-decorations.cs
@@ -98,7 +98,7 @@ class Decoration : Base, IInteractive, IBreakable
         }
         else if (isQuestItem)
         {
-            Console.WriteLine($"You look at the {this.name}. There is a key inside.");
+            Console.WriteLine($"You look at the {this.name}. There's a key inside.");
         }
         else if (!isQuestItem)
         {
