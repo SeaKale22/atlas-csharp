691d217af8c7c62ba7d22e583d0a62aecabac23c
SeaKale22 <kaleb.tahji.k@gmail.com>
2024-01-23 12:14:40 -0600

    cahnged incorrect type message

--- csharp-generics/5-concatenate/queue.cs
+++ csharp-generics/5-concatenate/queue.cs
@@ -111,7 +111,7 @@ class Queue<T>
         }
         if (typeof(T) != typeof(string) && typeof(T) != typeof(char))
         {
-            Console.WriteLine("Concatenate() is for a queue of Strings or Chars only.");
+            Console.WriteLine("Concatenate is for a queue of Strings or Chars only.");
             return null;
         }
         string result = this.head.value.ToString();
